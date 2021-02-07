1dfa0aa20cd3b6c4d3a110aeb4ff8bc2cf4b8b11
DionatanLeao <dionatan.ti@outlook.com>
2021-02-07 10:29:31 -0300

    Final commit

--- src/main/java/com/api/salareunioes/controller/RoomController.java
+++ src/main/java/com/api/salareunioes/controller/RoomController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,6 +68,7 @@ public class RoomController {
 		return ResponseEntity.ok(updateRoom);
 	}
 	
+	@DeleteMapping("/{id}")
 	public Map<String, Boolean> deleteRoom(@PathVariable(value="id") Long id) throws ResourceNotFoundException {
 		Room room = roomRepository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("Room not found: " + id));
